4d273fa64bc098bd7bbd1e8120d94700d690bb5a
Nathan-reitan <nathanreitan@gmail.com>
2024-07-12 11:46:03 -0600

    final adjustments

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -48,14 +48,14 @@ function LinkIcon(props) {
 
 export const metadata = {
   title: 'Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  description: 'A short history of my digital marketing journey',
 }
 
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="A short history of my digital marketing journey"
+      intro="I have contributed to numerous businesses and developed various personal projects, which have had a significant impact on several real-world enterprises."
     >
       <ul
         role="list"
